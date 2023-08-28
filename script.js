0ad8ec9f489c05616d24e49ceca1bdbc0289eb0a
unknown <marcosmonteiro28x@gmail.com>
2023-08-28 17:10:27 -0300

    :tada: Alterações finais

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function toggleMode() {
     //substituir a imagem
     if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-        img.setAttribute("src", "./assets/avatarM.png")
+        img.setAttribute("src", "./assets/avatar2.png")
     } else {
     // se tiver sem light mode, manter a imagem normal
-        img.setAttribute("src", "./assets/avatar2.png")
+        img.setAttribute("src", "./assets/avatarM.png")
     }
 }
